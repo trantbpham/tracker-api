ad652b29e164a76bbcf9ab8597693472002a5c13
Tran Pham <pham.tr@husky.neu.edu>
2020-07-23 20:31:45 -0400

    changed playground order

--- api_handler.js
+++ api_handler.js
@@ -34,12 +34,12 @@ const server = new ApolloServer({
   typeDefs: fs.readFileSync('schema.graphql', 'utf-8'),
   resolvers,
   context: getContext,
+    playground: true,
+  introspection: true,
   formatError: (error) => {
     console.log(error);
     return error;
   },
-  playground: true,
-  introspection: true,
 });
 
 function installHandler(app) {
